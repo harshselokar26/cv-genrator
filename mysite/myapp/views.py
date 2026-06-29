8c7aee48892912d31af57a5c349c7eda00f2a165
pknowledge <harshselokar07@gmail.com>
2026-06-29 12:09:02 +0530

    templates design complete

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Profile
 
-# Create your views here.
+
+def save_profile(request):
+    if request.method == "POST":
+
+        profile = Profile.objects.create(
+            name=request.POST.get("name"),
+            email=request.POST.get("email"),
+            phone=request.POST.get("phone"),
+            degree=request.POST.get("degree"),
+            school=request.POST.get("school"),
+            university=request.POST.get("university"),
+            summary=request.POST.get("summary"),
+            previous_work=request.POST.get("previous_work"),
+            skills=request.POST.get("skills"),
+        )
+
+        # Redirect to resume page
+        return redirect("resume", id=profile.id)
+
+    return render(request, "myapp/accept.html")
+
+
+def resume(request, id):
+    user_profile = Profile.objects.get(id=id)
+
+    return render(
+        request,
+        "myapp/resume.html",
+        {
+            "user_profile": user_profile
+        }
+    )
